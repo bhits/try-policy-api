19d863a5fab7be0176c0f40f488652dc3943d5f4
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2016-02-22 23:33:12 -0500

    Add transformation logic to convert segmentation xml to xhtml for trypolicy
    #8009 - As a developer, I can use a REST data segmentation service

--- tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/config/TryPolicyServicesConfig.java
+++ tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/config/TryPolicyServicesConfig.java
@@ -1,10 +1,19 @@
 package gov.samhsa.mhc.trypolicy.config;
 
 
+import gov.samhsa.mhc.common.document.converter.DocumentXmlConverter;
+import gov.samhsa.mhc.common.document.converter.DocumentXmlConverterImpl;
+import gov.samhsa.mhc.common.document.transformer.XmlTransformer;
+import gov.samhsa.mhc.common.document.transformer.XmlTransformerImpl;
+import gov.samhsa.mhc.common.marshaller.SimpleMarshaller;
+import gov.samhsa.mhc.common.marshaller.SimpleMarshallerImpl;
 import gov.samhsa.mhc.trypolicy.service.TryPolicyService;
 import gov.samhsa.mhc.trypolicy.service.TryPolicyServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class TryPolicyServicesConfig {
@@ -16,4 +25,23 @@ public class TryPolicyServicesConfig {
 
     @Bean
     public DSSProperties dssProperties(){ return new DSSProperties();}
+
+
+    @Bean
+    public DocumentXmlConverter documentXmlConverter() {
+        return new DocumentXmlConverterImpl();
+    }
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    @Autowired(required = true)
+    public XmlTransformer xmlTransformer(SimpleMarshaller marshaller) {
+        return new XmlTransformerImpl(marshaller);
+    }
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public SimpleMarshaller simpleMarshallerImpl() {
+        return new SimpleMarshallerImpl();
+    }
 }
--- tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/service/TryPolicyServiceImpl.java
+++ tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/service/TryPolicyServiceImpl.java
@@ -1,25 +1,56 @@
 package gov.samhsa.mhc.trypolicy.service;
 
+
+import gov.samhsa.mhc.common.document.converter.DocumentXmlConverter;
+import gov.samhsa.mhc.common.document.transformer.XmlTransformer;
+import gov.samhsa.mhc.common.param.Params;
 import gov.samhsa.mhc.trypolicy.config.DSSProperties;
 import gov.samhsa.mhc.trypolicy.exception.TryPolicyException;
 import gov.samhsa.mhc.trypolicy.service.dto.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.ProcessingInstruction;
 
+import javax.xml.transform.URIResolver;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class TryPolicyServiceImpl implements TryPolicyService {
+    /**
+     * The logger.
+     */
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     DSSProperties dssProperties;
 
+    @Autowired
+    DocumentXmlConverter documentXmlConverter;
+
+    @Autowired
+    private XmlTransformer xmlTransformer;
+
     @Override
     public String getSegmentDocXHTML(String patientUserName, String patientId, String documentId, String consentId, String purposeOfUseCode) throws TryPolicyException {
+
+        return getTaggedC32(getSegmentDocXML(patientUserName, patientId, documentId, consentId, purposeOfUseCode));
+
+    }
+
+
+
+    @Override
+    public String getSegmentDocXML(String patientUserName, String patientId, String documentId, String consentId, String purposeOfUseCode) throws TryPolicyException {
 
 
         RestTemplate restTemplate = new RestTemplate();
@@ -31,6 +62,40 @@ public class TryPolicyServiceImpl implements TryPolicyService {
 
         return invokeDssService(patientId, docStr, xacmlStr, purposeOfUseCode);
     }
+
+    private String getTaggedC32(String segmentedC32) {
+
+        final Document taggedC32Doc = documentXmlConverter
+                .loadDocument(segmentedC32);
+        changeXslPath(taggedC32Doc);
+
+        final NodeList taggedC32List = taggedC32Doc
+                .getElementsByTagName("entry");
+
+        final Document segmentedC32Doc = documentXmlConverter
+                .loadDocument(segmentedC32);
+        final NodeList segmentedC32List = segmentedC32Doc
+                .getElementsByTagName("entry");
+
+        //  logger.info("Original C32: " + originalC32);
+        logger.info("Segmented C32: " + segmentedC32);
+
+        logger.info("Tagged C32 Entry size: " + taggedC32List.getLength());
+        logger.info("Segmented C32 Entry size: "
+                + segmentedC32List.getLength());
+
+        // xslt transformation
+        final String xslUrl = Thread.currentThread()
+                .getContextClassLoader().getResource("CDA_flag_redact.xsl")
+                .toString();
+
+        final String output = xmlTransformer.transform(taggedC32Doc, xslUrl, Optional.<Params>empty(), Optional.<URIResolver>empty());
+
+        logger.info("Printing transformed xslt: " + output);
+        return output;
+    }
+
+
 
 
     private String invokeDssService(String patientId, String ccdStr, String xacmlStr, String purposeOfUse) {
@@ -81,5 +146,41 @@ public class TryPolicyServiceImpl implements TryPolicyService {
 
         return dssRequest;
     }
+
+    /**
+     * Changes xsl path to local xsl.
+     *
+     * @param taggedC32Doc the tagged c32 doc
+     */
+
+    private void changeXslPath(Document taggedC32Doc) {
+
+        final String expression = "/processing-instruction('xml-stylesheet')";
+        Optional<ProcessingInstruction> pi = Optional.empty();
+        /**
+         * Need to add xsl
+         */
+/*
+        try {
+            pi = documentAccessor.getProcessingInstruction(taggedC32Doc,
+                    expression);
+        } catch (final DocumentAccessorException e) {
+            throw new DSSException("Error processing xsl path");
+        }
+*/
+
+        // <?xml-stylesheet href="http://obhita.org/CDA.xsl" type="text/xsl"?>
+        if (pi.isPresent()) {
+            pi.get().setData("type='text/xsl' href='CDA_flag_redact.xsl'");
+        } else {
+            // Add xml style sheet at the second line of xml string
+            final ProcessingInstruction p = taggedC32Doc
+                    .createProcessingInstruction("xml-stylesheet",
+                            "type=\"text/xsl\" href=\"CDA_flag_redact.xsl\"");
+            final Element stylesheetEl = taggedC32Doc.getDocumentElement();
+            taggedC32Doc.insertBefore(p, stylesheetEl);
+        }
+    }
+
 
 }
