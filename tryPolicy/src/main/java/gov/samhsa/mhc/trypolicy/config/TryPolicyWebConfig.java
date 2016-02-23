a53aedf149400279d61bac5d57d77b3312502efb
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2016-02-23 16:30:11 -0500

    Add @EnableWebMvc annotation to serve static content
    #8009 - As a developer, I can use a REST data segmentation service

--- tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/config/TryPolicyWebConfig.java
+++ tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/config/TryPolicyWebConfig.java
@@ -2,12 +2,14 @@ package gov.samhsa.mhc.trypolicy.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by sadhana.chandra on 11/16/2015.
  */
 @Configuration
+@EnableWebMvc
 public class TryPolicyWebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
