1811e230e3ede00c671573d84bfebdd20b3b7f89
burcakulug <burcak.ulug@feisystems.com>
2016-03-24 16:40:43 -0400

    Configure try-policy as a discovery client
    #8417:As a developer, I can secure and discover MHC microservices

--- tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/TryPolicyApplication.java
+++ tryPolicy/src/main/java/gov/samhsa/mhc/trypolicy/TryPolicyApplication.java
@@ -2,14 +2,13 @@ package gov.samhsa.mhc.trypolicy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TryPolicyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TryPolicyApplication.class, args);
     }
-
-
-
 }
